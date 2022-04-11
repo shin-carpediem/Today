983804c675d79f1d916125dd5a2a1136765ea739
shin-carpediem <buru.aoshin@gmail.com>
2022-04-11 18:36:50 +0900

    first commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by shinichiro.aoki on 2022/04/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
