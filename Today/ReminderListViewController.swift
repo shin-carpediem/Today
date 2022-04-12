a1c9ff5933734c7c85b92db7534b4c31d6c1a2b5
shin-carpediem <buru.aoshin@gmail.com>
2022-04-12 15:52:21 +0900

    complete section 5

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad() /// 読み込み系ライフサイクルメソッド
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration{ (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: <#T##IndexPath#>, item: itemIdentifier)
+        }
     }
     
     /// ref https://qiita.com/ddd503/items/205002b44b7a22e5ba13
