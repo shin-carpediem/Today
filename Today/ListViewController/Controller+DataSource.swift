4ab807d7b0ec46f1f4b14141bcea31f53a9ca437
shin-carpediem <buru.aoshin@gmail.com>
2022-04-12 17:55:00 +0900

    complete section 2

---
+++ Today/ListViewController/Controller+DataSource.swift
@@ -0,0 +1,23 @@
+//
+//  Controller+DataSource.swift
+//  Today
+//
+//  Created by shinichiro.aoki on 2022/04/12.
+//
+
+import Foundation
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
