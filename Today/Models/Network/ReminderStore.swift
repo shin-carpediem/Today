a8b27766d7775784afa35b640a3e7f4db333e4c1
shin-carpediem <buru.aoshin@gmail.com>
2022-06-11 21:29:45 +0900

    imprement singleton datastore

---
+++ Today/Models/Network/ReminderStore.swift
@@ -0,0 +1,12 @@
+import Foundation
+import EventKit
+
+class ReminderStore {
+    static let shared = ReminderStore()
+    
+    private let ekStore = EKEventStore()
+    
+    var isAvailable: Bool {
+        EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+}
