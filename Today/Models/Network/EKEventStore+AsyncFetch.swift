9476c6ff437190fc398a633a8c601078b29640ce
shin-carpediem <buru.aoshin@gmail.com>
2022-06-11 21:26:52 +0900

    imprement initialization

---
+++ Today/Models/Network/EKEventStore+AsyncFetch.swift
@@ -0,0 +1,16 @@
+import Foundation
+import EventKit
+
+extension EKEventStore {
+    func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders = reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(throwing: TodayError.failedReadingReminders)
+                }
+            }
+        }
+    }
+}
--- Today/Models/Network/TodayError.swift
+++ Today/Models/Network/TodayError.swift
@@ -1,27 +1,15 @@
 import Foundation
-import EventKit
 
 enum TodayError: LocalizedError {
     case failedReadingReminders
+    case reminderHasNoDueDate
     
     var errorDescription: String? {
         switch self {
         case .failedReadingReminders:
             return NSLocalizedString("Failed to read reminders", comment: "failed reading reminders error description")
-        }
-    }
-}
-
-extension EKEventStore {
-    func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
-        try await withCheckedThrowingContinuation { continuation in
-            fetchReminders(matching: predicate) { reminders in
-                if let reminders = reminders {
-                    continuation.resume(returning: reminders)
-                } else {
-                    continuation.resume(throwing: TodayError.failedReadingReminders)
-                }
-            }
+        case .reminderHasNoDueDate:
+            return NSLocalizedString("A reminder has no due date.", comment: "reminder has no due date error description")
         }
     }
 }
