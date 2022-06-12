432ec92600bf72bc58e1840acead8f157a8c1a06
shin-carpediem <buru.aoshin@gmail.com>
2022-06-13 07:16:07 +0900

    update calendar

--- Today/Controllers/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/Controllers/ListViewController/ReminderListViewController+DataSource.swift
@@ -91,8 +91,14 @@ extension ReminderListViewController {
     }
     
     func update(_ reminder: Reminder, with id: Reminder.ID) {
-        let index = reminders.indexOfReminder(with: id)
-        reminders[index] = reminder
+        do {
+            try reminderStore.save(reminder)
+            let index = reminders.indexOfReminder(with: id)
+            reminders[index] = reminder
+        } catch TodayError.accessDenied {
+        } catch {
+            showError(error)
+        }
     }
     
     // MARK: - private
